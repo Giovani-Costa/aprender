cd20b8dc42318a096ec213f8257459133a1e8675
Giovani Costa <Giovanicostastudio@hotmail.com>
2022-07-25 19:37:38 -0300

    Desafio da primeira aula

--- cap_2_atividades.py
+++ cap_2_atividades.py
@@ -7,3 +7,56 @@ print(
 # Atividade 3
 sentimento = "bem"
 print(f"eu estou me sentindo {sentimento}")
+# Atividade 4
+haiku = """Adrienne enjoys
+Coffee,lots of coding, and
+Teaching you Python"""
+print(f"{haiku}")
+# Atividade 5
+# nome = input("nome: ")
+# adjetivo = input("adjetivo: ")
+# lanche = input("lanche: ")
+# numero = input("número: ")
+# arvore = input("tipo de árvore: ")
+# historia = f"""Oi, meu nome é {nome}.
+# Eu gosto muito de {lanche} {adjetivo}!
+# Eu gosto tanto que eu tento comer pelo menos {numero} vezes por dia.
+# O sabor é ainda melhor quando se comer sob um(a) {arvore}!"""
+# print(historia)
+# # Atividade 6
+# nome = input("nome: ")
+# sobrenome = input("sobrenome: ")
+# nome_completo = f"{nome} {sobrenome}"
+# print(nome_completo)
+# Atividade 7
+# Antes: 80 = "Adrienne"
+nome = "Adrienne"
+# Antes: 98_cookie_39 = "Chocolate chip cookies"
+cookie = "Cookie com gotas de chocolate"
+# Antes: fIrSt_NAMe = 20
+numero = 20
+# Antes: LAST_name = "Blue"
+cor = "Blue"
+# Antes: 309384 = "Adrienne Tacke"
+nome_completo = "Adrienne Tacke"
+# Antes: Hellllooooooooooooo_8392982r = "Software Engineer"
+profissao = "Desenvolvedor"
+# Desafio 1
+print(
+    """    (O)
+   (OOO)
+  (OOOOO)
+ (OOOOOOO)
+(OOOOOOOOO)"""
+)
+print(
+    """      @@@@
+      {  }
+     @@@@@@
+    {      }
+   @@@@@@@@@@
+ {            }
+    [**]
+  [******]
+ [*********]"""
+)
