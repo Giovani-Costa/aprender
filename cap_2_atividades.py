76e5467783ee50a1e77b5f17eee7ec97c34b37e1
Giovani Costa <Giovanicostastudio@hotmail.com>
2022-07-25 18:24:59 -0300

    Primeira aula de Python

---
+++ cap_2_atividades.py
@@ -0,0 +1,9 @@
+# Atividade 1
+print("oi meu nome é giovani")
+# Atividade 2
+print(
+    "'oq me preucupa n é o barulho dos maus, mas o silêncio dos bons'-Martin Luther King"
+)
+# Atividade 3
+sentimento = "bem"
+print(f"eu estou me sentindo {sentimento}")
---
+++ print_.py
@@ -0,0 +1,16 @@
+print("arroz")
+# teste
+print("'")
+print('"')
+print('"')
+print("feijao\naaa")
+print(
+    """batata
+mandioca"""
+)
+print("""quadrado""")
+farinha = "fermento"
+print(farinha)
+print(f"para fazer pão precisa de {farinha}")
+nome = input("escreva seu nome: ")
+print(f"ola {nome}")
