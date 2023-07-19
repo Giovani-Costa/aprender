ba8bacd680b1cbd6f898b774905963736d22b0e6
Giovani Costa <Giovanicostastudio@hotmail.com>
2023-07-18 21:47:23 -0300

    Melhorias no Bot

--- discord_bot/gp_bot.py
+++ discord_bot/gp_bot.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from key import key
+from random import choice
 
 intents = discord.Intents.default()
 intents.members = True
@@ -14,9 +15,26 @@ async def on_ready():
     print(f"{client.user} está online!")
 
 
+def escolher_usuario():
+    usuarios_ativos = [
+        "Yodah_",
+        "Pareozitas",
+        "Pedroda br",
+        "NIGHTWALL",
+        "Allan",
+        "Axel",
+        "Empaiors",
+        "FenixDumal",
+        "Vex",
+        "Ninguém",
+        "Alek do grau",
+        "Ninguém",
+    ]
+    return choice(usuarios_ativos)
+
+
 @client.event
 async def on_message(message):
-    numero_do_dado = random.randint(1, 6)
     conteudo = message.content
     conteudo_lower = conteudo.lower()
 
@@ -28,16 +46,43 @@ async def on_message(message):
 
     elif conteudo_lower.startswith("!regras"):
         await message.channel.send(
-            'As regras do serveridor são as seguinte:\n **1-** Você deve manter o "respeito", com os membros não ultrapassando os limites da zuera \n **2-** Você não pode spawmar mensagem para spawnar um pokémon em nenhum outro chat a não ser o Poke-chat \n **3**- Você não pode mandar qualquer coisa que faça referencia a coisas obcenas \n **4-** Você deve colocar áudios estorados em algum bot de músic \n **5-** Você não pode mandar nenhum vírus/malware na central de downloa \n **6-** Caso esteje participando do GP Server, não desrespeitar as regras. Para saber quais são digite: !GP Server regras'
+            """As regras do serveridor são as seguinte: 
+**1-** Você deve manter o "respeito", com os membros não ultrapassando os limites da zuera 
+**2-** Você não pode spawmar mensagem para spawnar um pokémon em nenhum outro chat a não ser o Poke-chat
+**3**- Você não pode mandar qualquer coisa que faça referencia a coisas obcenas
+**4-** Você deve colocar áudios estorados em algum bot de música
+**5-** Você não pode mandar nenhum vírus/malware na central de downloa
+**6-** Caso esteje participando do GP Server, não desrespeitar as regras. Para saber quais são digite: !GP Server regras"""
         )
 
     elif conteudo_lower.startswith("!gp server regras"):
         await message.channel.send(
-            "As regras do servidor do minecraft são as seguintes:\n **1-**Proibido o uso de quaisquer armaduras do mod Project E, em caso de estar jogando em um modpack\n **2-** Proibido o assassinato de qualquer pet de qualquer jogador\n **3-** Proibido qualquer tipo de várzea contra qualquer estrutura oficial em alguma vila principal\n **4-** Proibido usar recursos de outros jogadores sem consentimento ou trato\n **5-** Qualquer residência ou estrutura que esteja dentro da vila que foi construída e não esteja dentro da temática da mesma corre o risco de ser destruída\n **6-** Qualquer baú que não está próximo de uma residência ou estrutura pode ser roubado\n **7-**Proibido qualquer dano a uma estrutura oficial ou a uma residência\n **8-** Proibido usa qualquer comando para alterar o tempo do server"
+            """As regras do servidor do minecraft são as seguintes:
+**1-**Proibido o uso de quaisquer armaduras do mod Project E, em caso de estar jogando em um modpack
+**2-** Proibido o assassinato de qualquer pet de qualquer jogador
+**3-** Proibido qualquer tipo de várzea contra qualquer estrutura oficial em alguma vila principal
+**4-** Proibido usar recursos de outros jogadores sem consentimento ou trato
+**5-** Qualquer residência ou estrutura que esteja dentro da vila que foi construída e não esteja dentro da temática da mesma corre o risco de ser destruída
+**6-** Qualquer baú que não está próximo de uma residência ou estrutura pode ser roubado
+**7-**Proibido qualquer dano a uma estrutura oficial ou a uma residência
+**8-** Proibido usa qualquer comando para alterar o tempo do server"""
         )
-    elif conteudo_lower.startswith("!d6"):
+    elif conteudo_lower.startswith("!d"):
+        numero = int(conteudo_lower[2:])
         await message.channel.send(
-            f":game_die: Caiu no **{numero_do_dado}** :game_die:"
+            f":game_die: Caiu no **{random.randint(1, numero)}** :game_die:"
+        )
+    elif conteudo_lower.startswith("!cemitério"):
+        await message.channel.send(
+            f""":skull_crossbones: Quem morreu :skull_crossbones:: {escolher_usuario()}
+:cry: Quem sofreu com a perda :cry:: {escolher_usuario()}
+:partying_face: Quem comemorou a morte :partying_face:: {escolher_usuario()}
+:detective: Investigou a morte :detective:: {escolher_usuario()}
+:woman_detective: Ajudou a investigar :woman_detective:: {escolher_usuario()}
+:knife: Quem matou :knife:: {escolher_usuario()}
+:coffin: Esqueceu do velório :coffin:: {escolher_usuario()}
+:health_worker: Ressuscitou o morto :health_worker:: {escolher_usuario()}
+:man_zombie: Matou achando que era um zumbi :man_zombie:: {escolher_usuario()}"""
         )
 
 
